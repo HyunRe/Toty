bbd761ab8879f06ba451ed8032d7620fbc93942a
LeeJaehyung <540900@naver.com>
2025-01-15 15:57:59 +0900

    Fix: cicd pr 테스트용 주석 추가

--- src/main/java/com/toty/TotyApplication.java
+++ src/main/java/com/toty/TotyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Spring Security 비활성화
+// cicd pr 테스트
 public class TotyApplication {
 
 	public static void main(String[] args) {
