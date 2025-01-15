311662a764ca1a8d8d7d685cfc8d77f1478750e2
LeeJaehyung <540900@naver.com>
2025-01-15 15:02:58 +0900

    Feat: Spring Boot 세팅

---
+++ src/main/java/com/toty/TotyApplication.java
@@ -0,0 +1,15 @@
+package com.toty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Spring Security 비활성화
+public class TotyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toty/TotyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
