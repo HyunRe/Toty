6fdaab38a7af9fb72ed0b91052176c6f84f3a47f
LeeJaehyung <540900@naver.com>
2025-01-16 14:34:54 +0900

    Feat: DDD 패키지 구조 세팅 및 User 예시 코드 작성

---
+++ src/main/java/com/toty/user/application/UserService.java
@@ -0,0 +1,31 @@
+package com.toty.user.application;
+
+import com.toty.user.domain.User;
+import com.toty.user.domain.UserRepository;
+import com.toty.user.presentation.dto.request.UserSignUpRequest;
+import com.toty.user.presentation.dto.response.UserInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User signUp(UserSignUpRequest userSignUpRequest) {
+        if(userRepository.findByEmail(userSignUpRequest.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 사용자입니다.");
+        }
+
+        User newUser = new User(userSignUpRequest.getEmail(), userSignUpRequest.getPassword());
+        return userRepository.save(newUser);
+    }
+
+    public UserInfoResponse getUserInfo(Long userId) {
+        User foundUser = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        return new UserInfoResponse(foundUser.getEmail());
+    }
+}
