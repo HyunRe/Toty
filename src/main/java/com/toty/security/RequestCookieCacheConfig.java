fabde152c7c52c9c61ef783a16e6c97f777d1881
HyeranKeum <sarah2316@g.skku.edu>
2025-02-01 06:19:10 +0900

    Fix: CookieCacheConfig 추가

---
+++ src/main/java/com/toty/security/RequestCookieCacheConfig.java
@@ -0,0 +1,13 @@
+package com.toty.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.savedrequest.CookieRequestCache;
+
+@Configuration
+public class RequestCookieCacheConfig {
+    @Bean
+    public CookieRequestCache cookieRequestCache() {
+        return new CookieRequestCache();
+    }
+}
