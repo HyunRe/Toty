49cc2783bada126d4b2937a1a4296992de7bfbfc
HyeranKeum <sarah2316@g.skku.edu>
2025-02-03 06:44:05 +0900

    Fix: 시큐리티 필터 추가

---
+++ src/main/java/com/toty/jwt/RefreshTokenAuthenticationFilter.java
@@ -0,0 +1,92 @@
+package com.toty.jwt;
+
+import static com.toty.jwt.JwtTokenUtil.ACCESS_TOKEN_TTL;
+import static com.toty.jwt.JwtTokenUtil.REFRESH_TOKEN_TTL;
+
+import com.toty.redisConfig.RedisService;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.time.Duration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
+
+
+// 액세스 토큰 만료 시 리프레시 토큰 갱신 필터
+//@Component
+public class RefreshTokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+//    private final TokenProvider jwtTokenProvider;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    private String username;
+
+    public RefreshTokenAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil) {
+        super(new AntPathRequestMatcher("/api/auth/refresh", "GET")); // 특정 요청만 필터링
+        setAuthenticationManager(authenticationManager);
+//        this.jwtTokenProvider = jwtTokenProvider;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+            HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+
+        // 쿠키에 있는 리프레시 + 인증 토큰
+        // 인증토큰 -> 만료 확인, username까지 있는거 확인 -> refresh를 token으로 해서 넣기
+        Cookie[] cookies = request.getCookies();
+        String accessToken = "";
+        String refreshToken = "";
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                } else if (cookie.getName().equals("refreshToken")) {
+                    refreshToken = cookie.getValue();
+                }
+            }
+        }
+
+        username = "";
+        String foundRefreshToken = "";
+
+        try {
+            username = jwtTokenUtil.extractUsername(refreshToken);
+            System.out.println("username = " + username);
+        } catch (ExpiredJwtException e) {
+            username = "sarah23162@naver.com";
+        }
+
+        // 리프레시 토큰 유효성 검사
+        //Authentication auth = jwtTokenProvider.authenticate(new TokenAuthentication(username, refreshToken));
+        System.out.println("RefreshTokenAuthenticationFilter.attemptAuthentication");
+        return getAuthenticationManager().authenticate(new TokenAuthentication(username, refreshToken));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        // 새로 발급하고 response에 넣는 과정
+        String newAccessToken = jwtTokenUtil.generateToken(username, ACCESS_TOKEN_TTL);
+        String newRefreshToken = jwtTokenUtil.generateToken(username, REFRESH_TOKEN_TTL);
+
+        jwtTokenUtil.storeRefreshToken(username, newRefreshToken);
+
+        //응답에 쿠키 포함
+        response.addCookie(jwtTokenUtil.createCookie("accessToken", newAccessToken, false));
+        response.addCookie(jwtTokenUtil.createCookie("refreshToken", newRefreshToken, true));
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
