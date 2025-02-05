d4abc82512fb059d45b06e69d87ce0607c89885a
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2025-02-05 17:26:00 +0900

    채팅방 목록, 개수에따라 여러줄되게 하는거 구현함

--- src/main/resources/static/js/chatting/chatList.js
+++ src/main/resources/static/js/chatting/chatList.js
@@ -16,58 +16,52 @@ async function fetchRoomList() {
 function updateRoomListCd(roomList, loginId) {
     var roomCount = roomList.length;
     // 한줄에 몇개 넣을건지에 따라 결정되는 row수
-    var rowCount = Math.ceil(roomCount/4); // 수 올림
+    var rowCount = Math.ceil(roomCount/4);
 
+    // 채팅방 목록 들어가는 div
     const cardBox = document.querySelector('#chatRoomListBox');
     cardBox.innerHTML = ''; // 기존 내용을 초기화
 
-    // 일단 하나만 해보는거
-    let cardRow = document.createElement('div');
-    cardRow.className = "row";
-
-    let partList = roomList.slice(0,4);
-
-    partList.forEach(room => {
-        let roomComponent = document.createElement('div');
-        roomComponent.className = "col-3";
-        roomComponent.innerHTML = `           
-            <div class="card mb-2" >
-                <div class="row">
-                    <div class="col-4">
-                        <img alt="IMG">
-                    </div>
-                    <div class="col-8">
-                        <p>${room.mentor.userName}</p>
-                    </div>
-                </div>
-                <h4>${room.roomName}</h4>
-                <div>${room.createdAt}</div>
-                <div class="d-flex justify-content-between align-items-center">
-                    <span>현재인원/${room.userLimit} </span>
-                    <form action="/api/chatting/participant/${room.id}/${loginId}" 
-                        method="post">
-                        <button type="submit"> 단톡 참석 </button>
-                    </form>
-                </div>
-            </div>
-        `;
-        cardRow.appendChild(roomComponent);
-    });
-
-    cardBox.appendChild(cardRow);
-
-
+    // 채팅방들 들어있는 row들 추가하는 로직
     for (let i = 0; i < rowCount; i++) { 
-        // 여기서 slice로 잘라서 부분배열들마다 row아래에 col-3으로 넣어주는 로직해야함
-       
+        let cardRow = document.createElement('div');
+        cardRow.className = "row";
 
-        //cardBox.appendChild(cardRow);
-    }
+        // 배열을 4개씩 잘라서, 부분배열들마다 row아래에 col-3으로 넣어주는 로직
+        let partList = roomList.slice(4 * i, 4 * (i + 1));
+        partList.forEach(room => {
+            let roomComponent = document.createElement('div');
+            roomComponent.className = "col-3";
+            roomComponent.innerHTML = `           
+                <div class="card mb-2" >
+                    <div class="row">
+                        <div class="col-4">
+                            <img alt="IMG">
+                        </div>
+                        <div class="col-8">
+                            <p>${room.mentor.userName}</p>
+                        </div>
+                    </div>
+                    <h4>${room.roomName}</h4>
+                    <div>${room.createdAt}</div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span>현재인원/${room.userLimit} </span>
+                        <form action="/api/chatting/participant/${room.id}/${loginId}" 
+                            method="post">
+                            <button type="submit"> 단톡 참석 </button>
+                        </form>
+                    </div>
+                </div>
+            `;
+            cardRow.appendChild(roomComponent);
+        });
     
-
+        // 생성한 row를 박스에 넣는거
+        cardBox.appendChild(cardRow);
+    }
 }
 
-
+// 레거시 코드, 나중에 버릴꺼
 function updateRoomListTb(roomList, loginId) {
 
     const tableBody = document.querySelector('.table > tbody');
