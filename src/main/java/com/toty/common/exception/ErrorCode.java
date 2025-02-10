f81a2a510ea2766d34338a6e661ae91a569175f1
LeeJaehyung <540900@naver.com>
2025-02-10 12:02:54 +0900

    Fix: ErrorCode 컴파일 오류 수정

--- src/main/java/com/toty/common/exception/ErrorCode.java
+++ src/main/java/com/toty/common/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 검색
-    INVALID_SEARCH_FIELD(HttpStatus.BAD_REQUEST, "잘못된 검색 속성입니다.");
+    INVALID_SEARCH_FIELD(HttpStatus.BAD_REQUEST, "잘못된 검색 속성입니다."),
 
     // 사용자
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
