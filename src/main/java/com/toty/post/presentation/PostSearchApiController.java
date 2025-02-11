46e25019ae1a44c2a1e0aa2b0ee1ab1f66ee819e
LeeJaehyung <540900@naver.com>
2025-02-11 15:48:51 +0900

    Feat: search.html 페이지네이션 작업(css 제외)

--- src/main/java/com/toty/post/presentation/PostSearchApiController.java
+++ src/main/java/com/toty/post/presentation/PostSearchApiController.java
@@ -19,24 +19,46 @@ public class PostSearchApiController {
     private final PostSearchService postSearchService;
 
     /**
-     * 통합(일반 게시글, 정보 게시글, QnA 게시글) 검색 API
+     * 통합 검색 API
+     * 검색 시 처음 나오는 화면에 보여질 모든 게시판(일반, 정보, QnA)의 데이터들을 리턴합니다.
      *
      * @param keyword 검색어
      * @param field 검색 속성 (제목, 본문, 제목 + 본문)
-     * @param page 페이지 번호 (기본값: 1)
-     * @param size 페이지당 게시글 개수 (기본값: 10)
+     * @param size 페이지당 게시글 개수 (기본값: 5)
      * @return Map<게시판 종류(일반, 정보, QnA), Page<게시글(본문, 댓글 제외)>>
      */
     @GetMapping
     public TotyResponse<Map<PostCategory, Page<PostEs>>> searchPosts (
-            @RequestParam(name = "keyword") String keyword,
-            @RequestParam(name = "field", defaultValue = "TITLE") SearchField field,
-            @RequestParam(name = "page", defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(name = "keyword", defaultValue = "") String keyword,
+            @RequestParam(name = "field") SearchField field,
+            @RequestParam(defaultValue = "5") int size
     ) {
-        return TotyResponse.success(postSearchService.searchPosts(keyword, field, page, size));
+        return TotyResponse.success(postSearchService.searchPosts(keyword, field, size));
     }
 
+    /**
+     * 단일 검색 API
+     * 검색 후 나오는 화면에서 각 게시판의 페이지에 따라 데이터들을 리턴합니다.
+     *
+     * @param keyword 검색어
+     * @param field 검색 속성 (제목, 본문, 제목 + 본문)
+     * @param category 게시판 종류 (일반, 정보, QnA)
+     * @param page 페이지 번호 (기본값: 1)
+     * @param size 페이지당 게시글 개수 (기본값: 5)
+     * @return Map<게시판 종류(일반, 정보, QnA), Page<게시글(본문, 댓글 제외)>>
+     */
+    @GetMapping("/category")
+    public TotyResponse<Map<PostCategory, Page<PostEs>>> searchPostsByCategory (
+            @RequestParam(name = "keyword", defaultValue = "") String keyword,
+            @RequestParam(name = "field") SearchField field,
+            @RequestParam(name = "category") PostCategory category,
+            @RequestParam(name = "page") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        return TotyResponse.success(postSearchService.searchPostsByCategory(keyword, field, category, page, size));
+    }
+
+    // todo : 추후 게시글 작성 로직에 추가된 후 삭제될 API 입니다.
     @PostMapping("/create")
     public TotyResponse<String> createPost(
             @RequestParam(name = "title") String title,
