017ac7637ea9f45ca88dc5e42d00892edbc5ac0e
LeeJaehyung <540900@naver.com>
2025-02-12 10:48:53 +0900

    Fix: FcmConfig에서 FileInputStream 대신 ClassPathResource로 json 불러오도록 수정

--- src/main/java/com/toty/springconfig/fcm/FcmConfig.java
+++ src/main/java/com/toty/springconfig/fcm/FcmConfig.java
@@ -6,15 +6,17 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FcmConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/toty-6d4b4-firebase-adminsdk-fbsvc-f5eb24e27a.json");
+        InputStream serviceAccount = new ClassPathResource("toty-6d4b4-firebase-adminsdk-fbsvc-f5eb24e27a.json").getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
