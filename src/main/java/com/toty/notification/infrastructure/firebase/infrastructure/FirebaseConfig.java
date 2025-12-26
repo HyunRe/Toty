3b3d4aeaf379d8a73a020a380af401120dfdbf40
HyunRe <whwjdqls6868@naver.com>
2025-12-27 06:15:57 +0900

    firebase-adminsdk.json env로 이동

--- src/main/java/com/toty/notification/infrastructure/firebase/infrastructure/FirebaseConfig.java
+++ src/main/java/com/toty/notification/infrastructure/firebase/infrastructure/FirebaseConfig.java
@@ -6,28 +6,36 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
-    @Value("${firebase.service-account}")
-    private Resource serviceAccount;
+    @Value("${firebase.service-account-json}")
+    private String firebaseJsonBase64;
 
     @Value("${firebase.project-id}")
     private String projectId;
 
     @PostConstruct
-    public void init() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
-                    .setProjectId(projectId)
-                    .build();
+    public void init() throws Exception {
 
-            FirebaseApp.initializeApp(options);
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return;
         }
+
+        byte[] decoded = Base64.getDecoder().decode(firebaseJsonBase64);
+        InputStream is = new ByteArrayInputStream(decoded);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(is))
+                .setProjectId(projectId)
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 }
+
 
