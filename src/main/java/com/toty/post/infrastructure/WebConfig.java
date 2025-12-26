e1be12f98f633d386afa71d4e47a61e4aa3564b7
HyunRe <whwjdqls6868@naver.com>
2025-12-27 06:49:58 +0900

    ElasticSearchConfig 삭제

--- src/main/java/com/toty/post/infrastructure/WebConfig.java
+++ src/main/java/com/toty/post/infrastructure/WebConfig.java
@@ -1,4 +1,4 @@
-package com.toty.post.infrastructure.web;
+package com.toty.post.infrastructure;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
